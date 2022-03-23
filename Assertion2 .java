cfc45c3aa85bd9a2fdd7c82eb021ecb8e3752fb8
Eishitha <102133159+Eishitha@users.noreply.github.com>
2022-03-23 20:10:04 +0530

    Create Assertion2 .java

---
+++ Assertion2 .java
@@ -0,0 +1,9 @@
+# 21-3-2022-java-programs
+package com.example.sahitya.abcd;
+
+public class Assertion2 {
+    public static void main(String[] args) {
+     String[] chocolates = {"Dairy Milk","5 Star","Perk"};
+     assert chocolates.length==2:"Only 2 chocolates are available in the store";
+    }
+}
